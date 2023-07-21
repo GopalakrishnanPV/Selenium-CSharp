20e8927bbe11db9bdcd947e2cbe037f7327f9cc1
Panchavarnam Gopalakrishnan <gopalakrishnan.panchavarnam@mt.com>
2023-07-21 21:03:17 +0530

    Reporting and logging complete.

--- reporting-and-logging/Program.cs
+++ reporting-and-logging/Program.cs
@@ -1,4 +1,7 @@
-﻿using log4net;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using log4net;
+using OpenQA.Selenium.Chrome;
 
 namespace reporting_and_logging
 {
@@ -6,8 +9,43 @@ namespace reporting_and_logging
 	{
 		static void Main(string[] args)
 		{
-			ILog log = LogManager.GetLogger(typeof(Program));
-			log.Debug("Test");
+			// // Logging with log4net
+			//ILog log = LogManager.GetLogger(typeof(Program));
+			//log.Debug("This is log4net debug");
+			//log.Info("This is log4net info");
+			//log.Error("This is log4net error");
+
+			// Reporting
+			ExtentReports extentReports = new ExtentReports();
+			ExtentHtmlReporter extentHtmlReporter = new ExtentHtmlReporter("D://reports//index.html");
+			extentReports.AttachReporter(extentHtmlReporter);
+			ExtentTest loginTest = extentReports.CreateTest("Login Test");
+
+			try
+			{
+				ChromeDriver driver = new ChromeDriver();
+				loginTest.Log(Status.Pass, "Launch chrome driver successful");
+				driver.Manage().Window.Maximize();
+				loginTest.Log(Status.Pass, "Browser maximized");
+				driver.Url = "https://auto-bot-demo.vercel.app";
+				loginTest.Log(Status.Pass, "Navigated to practice website");
+				// Simulation of an error.
+				throw new Exception("Could not close browser");
+				driver.Close();
+				loginTest.Log(Status.Pass, "Driver closed");
+			}
+			catch (Exception ex)
+			{
+				loginTest.Log(Status.Fail, "Scenario failed with an exception: " + ex.Message);
+			}
+			finally
+			{
+				extentReports.Flush();
+			}
+
+
+			
+
 		}
 	}
 }
