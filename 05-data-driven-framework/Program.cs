f959a59df45509d3a9c2ebff6f8de4d37447eac6
Panchavarnam Gopalakrishnan <gopalakrishnan.panchavarnam@mt.com>
2023-07-23 19:22:59 +0530

    Data driven changes

---
+++ 05-data-driven-framework/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace _05_data_driven_framework
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IWebDriver driver = new ChromeDriver();
+			driver.Manage().Window.Maximize();
+			driver.Url = "https://www.auto-bot-demo.vercel.app";
+			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+			HomePage homePage = new HomePage(driver);
+			homePage.EnterName("Gopal");
+			homePage.EnterAge(18);
+			homePage.EnterEmail("gopal@test.com");
+		}
+	}
+}
