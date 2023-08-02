86298f4695ff08aea82b6aae3e277449aa45b2a2
Panchavarnam Gopalakrishnan <gopalakrishnan.panchavarnam@mt.com>
2023-08-02 23:56:37 +0530

    Complete Excel Data read

--- 05-data-driven-framework/ExcelDataAccess.cs
+++ 05-data-driven-framework/ExcelDataAccess.cs
@@ -1,28 +1,56 @@
 ﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace _05_data_driven_framework
 {
 	internal class ExcelDataAccess
 	{
-		public static string TestDataFileConnection()
+		public static string ReadCellValue(string filePath, string cellAddress)
 		{
-			var fileName = ConfigurationManager.AppSettings["TestDataSheetPath"];
-			var con = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source = {0}; Extended Properties=Excel 12.0;", fileName);
-			return con;
+			using (var wbook = new XLWorkbook(filePath))
+			{
+				var wsheet = wbook.Worksheet(1);
+				var data = wsheet.Cell(cellAddress).GetValue<string>();
+				return data;
+			}
 		}
 
-		public static HomepageData? GetTestData(string keyName)
+		public static DataSet ReadExcelAsDataTable(string filePath)
 		{
-			//using (var connection = new OleDbConnection(TestDataFileConnection()))
-			//{
-			//	connection.Open();
-			//	var query = string.Format("select * from [DataSet$] where key='{0}'", keyName);
-			//	var value = connection.Query<HomepageData>(query).FirstOrDefault();
-			//	connection.Close();
-			//	return value;
-			//}
-			return null;
+			DataSet dataSet = new DataSet();
+			using(var wbook = new XLWorkbook(filePath))
+			{
+				//var wsheet = wbook.Worksheet(1);
+				foreach(IXLWorksheet wsheet in wbook.Worksheets)
+				{
+					DataTable dt = new DataTable(wsheet.Name);
+
+					// Headers
+					wsheet.FirstRowUsed().CellsUsed().ToList()
+						.ForEach(x => { dt.Columns.Add(x.Value.ToString()); });
+
+					// Values
+					foreach (IXLRow row in wsheet.RowsUsed().Skip(1))
+					{
+						DataRow dataRow = dt.NewRow();
+						for (int i = 0; i < dt.Columns.Count; i++)
+						{
+							dataRow[i] = row.Cell(i + 1).Value.ToString();
+						}
+						dt.Rows.Add(dataRow);
+
+					}
+					dataSet.Tables.Add(dt);
+				}
+			
+			}
+
+			return dataSet;
 		}
 	}
 }
--- 05-data-driven-framework/Program.cs
+++ 05-data-driven-framework/Program.cs
@@ -1,6 +1,8 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using System.Data;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace _05_data_driven_framework
 {
@@ -8,7 +10,29 @@ namespace _05_data_driven_framework
 	{
 		static void Main(string[] args)
 		{
-			
+			IWebDriver driver = new ChromeDriver();
+			driver.Manage().Window.Maximize();
+			driver.Url = "https://auto-bot-demo.vercel.app/";
+			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+
+			//var name = ExcelDataAccess.ReadCellValue("Data\\HomepageData.xlsx", "A2");
+			//var age = ExcelDataAccess.ReadCellValue("Data\\HomepageData.xlsx", "B2");
+			//var email = ExcelDataAccess.ReadCellValue("Data\\HomepageData.xlsx", "C2");
+
+			HomePage homePage = new HomePage(driver);
+
+
+			var dataSet = ExcelDataAccess.ReadExcelAsDataTable("Data\\HomepageData.xlsx");
+
+			foreach (DataRow row in dataSet.Tables["Sheet1"].Rows)
+			{
+				homePage.EnterName(row["Name"].ToString());
+				homePage.EnterAge(int.Parse(row["Age"].ToString()));
+				homePage.EnterEmail(row["Email"].ToString());
+
+			}
+
 		}
 	}
 }
